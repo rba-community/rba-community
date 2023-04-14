fce21385bf79448977f1d8793cd1b1287ea031e5
Zachary Christensen <zchristensen@splunk.com>
2023-04-14 12:55:58 -0600

    added rum
    Signed-off-by: Zachary Christensen <zchristensen@splunk.com>

--- docs/javascripts/rum.js
+++
@@ -1,9 +0,0 @@
-<script src="https://cdn.signalfx.com/o11y-gdi-rum/latest/splunk-otel-web.js" crossorigin="anonymous"></script>
-<script>
-    SplunkRum.init({
-        beaconUrl: "https://rum-ingest.us1.signalfx.com/v1/rum",
-        rumAuth: "EK5fN_cQS_j2hNVDgVCMvA",
-        app: "RBA_Website",
-        environment: "prod"
-        });
-</script>
